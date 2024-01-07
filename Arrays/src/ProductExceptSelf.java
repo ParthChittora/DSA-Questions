05285e805487ff6881dcd39deb9f9699612fde13
Parth Chittora <chittora14parth@gmail.com>
2024-01-07 17:30:07 +0530

    Comments added in product except self

--- Arrays/src/ProductExceptSelf.java
+++ Arrays/src/ProductExceptSelf.java
@@ -7,10 +7,13 @@ public class ProductExceptSelf {
         int[] lefts = new int[n];
         int[] rights = new int[n];
         int[] result = new int[n];
+        // putting the first el of left array 1 as the left product of the first el of nums would be 1
+        // Then we calculate the left product of each el from index 1
         lefts[0] = 1;
         for(int i=1; i<n; i++){
             lefts[i] = lefts[i-1] * nums[i-1];
         }
+        // calculating right side product from last 
         rights[n-1] = 1;
         for(int i=n-2; i>=0; i--){
             rights[i] = rights[i+1] * nums[i+1];
@@ -24,6 +27,5 @@ public class ProductExceptSelf {
     public static void main(String[] args) {
         int[] sample = {1,2,0,4};
         System.out.println(Arrays.toString(productExceptSelf(sample)));
-z
     }
 }
