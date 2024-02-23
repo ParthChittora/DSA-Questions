b92905e3723aa676768e74c71bd8b739b08b5530
Parth Chittora <chittora14parth@gmail.com>
2024-02-23 15:28:38 +0530

    Goal Parser modified

--- Strings/src/GoalParserInterpretion.java
+++ Strings/src/GoalParserInterpretion.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class GoalParserInterpretion {
+public class GoalParserInterpretion {
+    public static String interpret(String command) {
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i <command.length() ; i++) {
+            if (command.charAt(i) == 'G') {
+                ans.append('G');
+            }
+            else if (command.charAt(i)=='('&&command.charAt(i+1)==')'){
+                ans.append('o');
+                i++;
+            }
+            else{
+                ans.append("al");
+                i+=3;
+            }
+
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        String ip = "G()(al)()()";
+        System.out.println(interpret(ip));
+    }
 }
