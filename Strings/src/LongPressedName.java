9f2a944db5aeed67b45cb589f4f74565d5b13b91
Parth Chittora <chittora14parth@gmail.com>
2024-02-27 23:04:10 +0530

    Long Pressed Name and Valid Palindrome II file added soln to be solved

--- Strings/src/LongPressedName.java
+++ Strings/src/LongPressedName.java
@@ -1,19 +1,15 @@
-public class ValidPalindrome {
-    public static boolean isPalindrome(String s) {
-        String helper = s.toLowerCase();
-        char[]arr =helper.toCharArray();
-        StringBuilder ans = new StringBuilder();
-        for(char el : arr){
-            if(el>='a'&&el<='z'){
-                ans.append(el);
+public class LongPressedName {
+    public static boolean isLongPressedName(String name, String typed) {
+        for (int i = 0; i <typed.length() ; i++) {
+            if(typed.charAt(i)==typed.charAt(i+1)){
+                return typed.startsWith(name,i);
             }
         }
-        System.out.println(ans);
         return false;
     }
-
     public static void main(String[] args) {
-        String a = "race a car";
-        System.out.println(isPalindrome(a));
+        String a = "alex";
+        String b = "aalex";
+        System.out.println(isLongPressedName(a,b));
     }
 }
---
+++ Strings/src/ValidPalindromeII.java
@@ -0,0 +1,13 @@
+public class ValidPalindromeII {
+    public  static boolean validPalindrome(String s) {
+        StringBuilder st = new StringBuilder();
+        st.append(s);
+
+        
+    }
+
+    public static void main(String[] args) {
+        String s = "abca";
+        System.out.println(validPalindrome(s));
+    }
+}
